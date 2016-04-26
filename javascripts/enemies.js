d06329d343d14c55628372da71dc17e0f62156c3
Ryan B <ryan.bliton@gmail.com>
2016-04-25 20:42:01 -0500

    enemy now assigned a random instructor name

--- javascripts/enemies.js
+++ javascripts/enemies.js
@@ -2,10 +2,18 @@
 var Gauntlet = (function(OrigGauntlet){
 
   OrigGauntlet.Combatants.Orc = function() {
+    this.name = null;
     this.health = this.health + 20;
     this.species = "Orc";
     this.allowedClasses = ["Warrior", "Berserker", "Shaman"];
     this.allowedWeapons = ["WarAxe", "Wand", "Staff"];
+    this.allowedNames = ["Joe", "Zoe", "Steve", "Greg"];
+    this.pickName = function() {
+      var random = Math.round(Math.random() * (this.allowedNames.length - 1));
+
+      this.name = this.allowedNames[random]
+      console.log("name", this.name );
+    };
     this.generateClass = function() {
       // Get a random index from the allowed classes array
       var random = Math.round(Math.random() * (this.allowedClasses.length - 1));
@@ -16,8 +24,6 @@ var Gauntlet = (function(OrigGauntlet){
       // Composes the corresponding player class into the player object
       this.class = new OrigGauntlet.GuildHall[randomClass]();
       return this.class;
-
-
     };
   };
 
